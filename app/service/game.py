e525f49933866d47f264345d0eb5bbab0380beef
Yevhenii Dermenzhii <dzhonu@ukr.net>
2022-08-06 10:44:02 +0300

    Implementing WS API

--- app/service/game.py
+++ app/service/game.py
@@ -1,10 +1,15 @@
-import uuid
+import secrets
+import string
 
 from common.model import NewGameRequest, NewGameResponse
 from storage import db
 
 
+def random_game_id():
+    return ''.join(secrets.choice(string.ascii_lowercase + string.digits) for _ in range(4))
+
+
 def new_game(req: NewGameRequest) -> NewGameResponse:
-    game_id = str(uuid.uuid4())
+    game_id = random_game_id()
     db.create_game(game_id, req.userId)
     return NewGameResponse(game_id)
