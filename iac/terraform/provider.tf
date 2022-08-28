31ae394ca7c1c75bbf76e87036dd77cde4f7c2cc
Yevhenii Dermenzhii <dzhonu@ukr.net>
2022-08-29 00:07:09 +0300

    Clean up

--- iac/terraform/provider.tf
+++ iac/terraform/provider.tf
@@ -6,15 +6,3 @@ provider "aws" {
   region  = var.region
   profile = var.profile
 }
-
-provider "aws" {
-  alias   = "eu-west-1"
-  region  = "eu-west-1"
-  profile = var.profile
-}
-
-provider "aws" {
-  alias   = "ap-southeast-1"
-  region  = "ap-southeast-1"
-  profile = var.profile
-}
